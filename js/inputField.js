36948483109e2970edc4586141c8cfcc7e6bb009
EarthenSky <earthenskymath@gmail.com>
2017-09-10 14:51:49 -0700

    added info and text cursor

--- js/inputField.js
+++ js/inputField.js
@@ -1,4 +1,49 @@
 function start() {
   console.log('start');
-  //$("#pageMid").hide();  //This is jQuery
+
+  //This is jQuery
+  $("#pageMidInfo").hide();
+  $("textCursor").hide();
+}
+
+var textSelected = false;
+function toggleTextSelected() { //?
+  textSelected = !textSelected;  //place this at onClick();
+
+  if(textSelected) {
+    startBlinkyTextCursorAnimation();
+  }
+  else {
+    clearTimeout(blinkyTextCursorID);
+    //hide cursor
+    $("textCursor").hide();
+    cursorOn = false
+  }
+}
+
+var cursorOn = false;
+var blinkyTextCursorID;
+function startBlinkyTextCursorAnimation() {
+  cursorOn = !cursorOn;
+
+  if(cursorOn) {
+    $("textCursor").show();
+  }
+  else {
+    $("textCursor").hide();
+  }
+
+  blinkyTextCursorID = setTimeout(startBlinkyTextCursorAnimation, 400);
+}
+
+var infoHidden = true;
+function toggleInfo() {
+  infoHidden =! infoHidden;
+
+  if(infoHidden) {
+    $("#pageMidInfo").hide();
+  }
+  else {
+    $("#pageMidInfo").show();
+  }
 }
