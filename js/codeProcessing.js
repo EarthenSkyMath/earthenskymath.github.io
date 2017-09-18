55780f1e679ce438da72fdec4b27e6e363b50463
EarthenSky <earthenskymath@gmail.com>
2017-09-17 21:28:32 -0700

    some more variable work (not tested)

--- js/codeProcessing.js
+++ js/codeProcessing.js
@@ -10,9 +10,10 @@ var processStageEnum = {
     AFTERKEY : 1, //ignores each char until it hits whitespace
     OUT : 2,      //print to the output
     SI_KEYS : 3,  //Second Instance Keywords (start looking for what type of variable is being defined)
-    PUSH : 4,     //call a function
-    COM : 5,      //comment line
-    ENDLINE : 6   //this line of code is done
+    DEFINE : 4,  //variable syntax.
+    PUSH : 5,     //call a function
+    COM : 6,      //comment line
+    ENDLINE : 7   //this line of code is done
 }
 var nextProcessStage;  //used for queing a processStage
 var processStage = processStageEnum.FI_KEYS;
@@ -50,6 +51,9 @@ function runCode() {
         break;
       case processStageEnum.SI_KEYS:
         checkSIKeywords();
+        break;
+      case processStageEnum.DEFINE:
+        variableType();
         break;
       default:
         alert("Ahahahhahaha!  Something broke.  This is never supposed to be called.  (^ w ^)");
@@ -99,7 +103,7 @@ function checkFIKeywords() {
 //ignore any character until whitespace
 function afterKeywords() {
   switch(code[i]) {
-    case " ":
+    case " ": case String.fromCharCode(10): //10 is \n
       processStage = nextProcessStage;
       nextProcessStage = undefined;
       break;
@@ -162,31 +166,100 @@ function comment() {
   }
 }
 
+//this says what varible is being defined
+var varTypeEnum = {
+    WEB : 0,    //holds all kinds of values
+    INT : 1,    //integer type
+    FLOAT : 2,  //floating point value
+    STRING : 3, //there is no char type, just small strings
+    BOOL : 4,   //boolean type
+}
+var varType;  //used to define var type
 function checkSIKeywords() {
   switch(code[i]) {
     case " ": case String.fromCharCode(10): //10 is \n
       ignoreChar();
       break;
-    case "=":
-      processStage = processStageEnum.AFTERKEY;
-      nextProcessStage = processStageEnum.OUT;
-      break;
-    case "c":
-      processStage = processStageEnum.AFTERKEY;
-      nextProcessStage = processStageEnum.COM;
-      break;
-    case "d":
+    case "w":  //web
+      varType = varTypeEnum.WEB;
       processStage = processStageEnum.AFTERKEY;
       nextProcessStage = processStageEnum.DEFINE;
       break;
-    case "p":
-
+    case "i":  //int
+      varType = varTypeEnum.INTEGER;
+      processStage = processStageEnum.AFTERKEY;
+      nextProcessStage = processStageEnum.DEFINE;
+      break;
+    case "f":  //float
+      varType = varTypeEnum.FLOAT;
+      processStage = processStageEnum.AFTERKEY;
+      nextProcessStage = processStageEnum.DEFINE;
+      break;
+    case "s":  //string
+      varType = varTypeEnum.STRING;
+      processStage = processStageEnum.AFTERKEY;
+      nextProcessStage = processStageEnum.DEFINE;
+      break;
+    case "b":  //bool
+      varType = varTypeEnum.BOOL;
+      processStage = processStageEnum.AFTERKEY;
+      nextProcessStage = processStageEnum.DEFINE;
       break;
     default:
       exitProgram();
       output.innerHTML += "&nbsp; ER:3 | Incorrect type defined at line : " + lineNumber + " <br>";
       return;
   }
+}
+
+//chooses which variable syntax should be followed
+function variableType() {
+  switch(varTypeEnum) {
+    case varTypeEnum.WEB:  //web
+      webVar();
+      break;
+    case varTypeEnum.INTEGER:  //int
+      intVar();
+      break;
+    case varTypeEnum.FLOAT:  //float
+      floatVar();
+      break;
+    case varTypeEnum.STRING:  //string
+      stringVar();
+      break;
+    case varTypeEnum.BOOL:  //bool
+      boolVar();
+      break;
+    default:
+      exitProgram();
+      output.innerHTML += "&nbsp; ER:3 | Incorrect type defined at line : " + lineNumber + " <br>";
+      return;
+  }
+}
+
+//web syntax
+function webVar() {
+
+}
+
+//int syntax
+function intVar() {
+
+}
+
+//float syntax
+function floatVar() {
+
+}
+
+//string syntax
+function stringVar() {
+
+}
+
+//bool syntax
+function boolVar() {
+
 }
 
 function ignoreChar() {
